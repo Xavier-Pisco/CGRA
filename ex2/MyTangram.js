8d06386656c89b7df0ebfe6fdc5f745eb9efcf31
Xavier-Pisco <x.pisco@gmail.com>
2020-02-26 15:58:07 +0000

    Changed folders and started ex2

---
+++ ex2/MyTangram.js
@@ -0,0 +1,15 @@
+/**
+ * MyTangram
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+
+class MyTriangle extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+    }
+    initBuffers() {
+		
+	}
+}
